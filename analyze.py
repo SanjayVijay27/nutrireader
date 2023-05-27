16d3a99c09457bbfe5413f7baf866997b7949f78
SanjayVijay27 <27sanjayvijay@gmail.com>
2023-05-27 16:00:18 -0400

    changed analyze.py to account for beverages

--- analyze.py
+++ analyze.py
@@ -9,13 +9,22 @@ openai.api_key = os.getenv("GPT_KEY")
 def get_info(serv_size, cals, sat_fat, sodium, fiber, sugar, protein, beverage):
     #Get preliminary subscore for each food item
     features = {}
-    features["energy_density"] = cals / serv_size * 100 / 80
-    features["sugar"] = sugar / serv_size * 100 / 4.5
-    features["sat_fat"] = sat_fat / serv_size * 100
-    features["salt"] = sodium / serv_size * 100 / 90
+    
+    if beverage:
+        #Convert ingredients into unit per hundred milliliters and divide by constant
+        features["energy_density"] = cals / serv_size * 100 / 7.15    #kilocalories
+        features["sugar"] = sugar / serv_size * 100 / 1.5   #grams
+    else:
+        #Convert ingredients into unit per hundred grams and divide by constant
+        features["energy_density"] = cals / serv_size * 100 / 80    #kilocalories
+        features["sugar"] = sugar / serv_size * 100 / 4.5   #grams
 
-    features["fiber"] = fiber / serv_size * 100 / 0.7
-    features["protein"] = protein / serv_size * 100 / 1.6
+    #Constant is the same for beverages and non-beverages
+    features["sat_fat"] = sat_fat / serv_size * 100 #grams
+    features["salt"] = sodium / serv_size * 100 / 90    #grams
+
+    features["fiber"] = fiber / serv_size * 100 / 0.7   #grams
+    features["protein"] = protein / serv_size * 100 / 1.6   #grams
 
     #Cap subscores to maximum values
     for key in features:
